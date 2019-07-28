736b4fd54f223ede5ae4511a5f0bb683ea6b0523
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-07-28 11:46:14 +0530

    update

---
+++ codechef/SFRV.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T,N;
+    cin>>T;
+    while(T--){
+        cin>>N;
+        for(int i=0; i<N; i++){
+            
+        }
+    }
+        
+    return 0;
+}
