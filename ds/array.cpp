f38a9a13bdb755afa61a266bacd78ddf60b556f0
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-07-31 23:51:44 +0530

    updates

--- ds/array.cpp
+++ ds/array.cpp
@@ -6,8 +6,21 @@ using namespace std;
 
 #define N 20
 int main(){
-    int a[N] = {};
-    for(int i=0; i<N; i++){
-        cout<<a[i]<<endl;
-    }
+    // int a[N] = {};
+    // for(int i=0; i<N; i++) cout<<a[i]<<endl;
+    // for(int elem:a) cout<<elem<<endl;
+    int num;cout<<num<<" "<<&num<<"\n";
+    int * p;cout<<p<<" "<<&p<<" "<<*p<<"\n";
+    p = &num;cout<<p<<" "<<&p<<" "<<*p<<"\n";
+    num = 10;cout<<p<<" "<<&p<<" "<<*p<<"\n";
+
+    int*q = &num;
+    cout<<q<<" "<<&q<<" "<<*q<<"\n";
+
+    int* r = new int(3);
+    cout<<r<<" "<<&r<<" "<<*r<<"\n";
+
+
+
+
 }
