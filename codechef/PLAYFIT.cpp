f489e1440e005d96a7e7f4b81b866fd38df15da3
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-08-01 20:40:09 +0530

    updates

---
+++ codechef/PLAYFIT.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+// //TLE
+// int main(){
+//     int T,N;
+//     cin>>T;
+//     while(T--){
+//         cin>>N;
+//         int G[N];
+//         for(int i=0; i<N; i++){
+//             cin>>G[i];
+//         }
+//         int maxdiff=0;
+//         for(int i=0; i<N; i++){
+//             for(int j=i+1; j<N; j++){
+//                 if(G[j]-G[i]>maxdiff) maxdiff = G[j]-G[i];
+//             }
+//         }
+//         if(maxdiff==0) cout<<"UNFIT"<<"\n";
+//         else cout<<maxdiff<<"\n";
+//     }
+
+// }
+
+int main(){
+    int T,N;
+    cin>>T;
+    while(T--){
+        cin>>N;
+        int G[N];
+        for(int i=0; i<N; i++){
+            cin>>G[i];
+        }
+    }
+}
