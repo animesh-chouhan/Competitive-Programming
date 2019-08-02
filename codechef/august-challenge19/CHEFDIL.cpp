e103b8dc96bded338ff4c1595438578f128a1c20
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-08-02 20:11:21 +0530

    updates

--- codechef/august-challenge19/CHEFDIL.cpp
+++ codechef/august-challenge19/CHEFDIL.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <string.h> 
 
 using namespace std;
 
 #define lli long long int 
 
+bool game(string str){
+    int l = str.length();
+    bool flag1 = true;
+    for(int i=0; i<l; i++) if(str[i]==1) flag1 = false;
+    if(flag1==true) return true;
+    else{
+        for(int i=0; i<l; i++){
+        if(str[i]==1){
+            bool flag2 = game(str.substr(0,i)) && game(str.substr(i+1, l-i-1));
+            }
+        }
+    }
+}
+
+
 int main(){
     lli t;
     cin>>t;
     while(t--){
-        cin>>n;
+        string str;
+        getline (cin, str); 
+        if(game(str)) cout<<"WIN"<<endl;
+        else cout<<"LOSE"<<endl;
+        
     }
+    return 0;
 }
