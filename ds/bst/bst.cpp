55c5c0d22b7febac6ccf5b48de8d5fc8525743d1
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-08-23 17:54:42 +0530

    updates

---
+++ ds/bst/bst.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+struct node
+{
+    int val;
+    node *right;
+    node *left;
+    node *parent;
+};
+
+void insert(int val, node *parent)
+{   
+    // cout<<val<<" "<<parent->val<<"\n";
+    if (parent->val == -1)
+    {
+        parent->val = val;
+        return;
+    }    
+    
+    if (val >= parent->val)
+    {   
+        if (parent->right == nullptr)
+        {   
+            node* right = new node;
+            right->val = val;
+            right->parent = parent;
+
+            parent->right = right;
+        }
+        else
+        {   
+            insert(val, parent->right);
+        }
+    }
+
+    else
+    {
+        if (parent->left == nullptr)
+        {
+            node* left = new node;
+            left->val = val;
+            left->parent = parent;
+
+            parent->left = left;
+        }
+        else
+        {
+            insert(val, parent->left);
+        }
+    }
+}
+
+void inorder(node *root)
+{
+    // cout<<root->val<<"\n";
+    if(root->right!=nullptr)
+        inorder(root->right);
+    cout<<root->val<<"\n";
+    if(root->left!=nullptr)
+        inorder(root->left);
+    // cout<<root->val<<"\n";
+}
+
+int main()
+{
+    int a[] = {3, 4, 1, 2, 5, 9, 7};
+    int n = sizeof(a)/sizeof(a[0]);
+
+    node root;
+    root.val = -1;
+    root.left = nullptr;
+    root.right = nullptr;
+    root.parent = nullptr;
+
+    for(int i=0;i<n; i++)   insert(a[i], &root);
+
+    inorder(&root);
+
+}
