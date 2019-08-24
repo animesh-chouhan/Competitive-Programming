b63e135614c9dc5cc293904093d556e5fec0968c
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-08-24 11:20:10 +0530

    updates

--- ds/bst/bst.cpp
+++ ds/bst/bst.cpp
@@ -12,26 +12,26 @@ struct node
 };
 
 void insert(int val, node *parent)
-{   
+{
     // cout<<val<<" "<<parent->val<<"\n";
     if (parent->val == -1)
     {
         parent->val = val;
         return;
-    }    
-    
+    }
+
     if (val >= parent->val)
-    {   
+    {
         if (parent->right == nullptr)
-        {   
-            node* right = new node;
+        {
+            node *right = new node;
             right->val = val;
             right->parent = parent;
 
             parent->right = right;
         }
         else
-        {   
+        {
             insert(val, parent->right);
         }
     }
@@ -40,7 +40,7 @@ void insert(int val, node *parent)
     {
         if (parent->left == nullptr)
         {
-            node* left = new node;
+            node *left = new node;
             left->val = val;
             left->parent = parent;
 
@@ -55,32 +55,64 @@ void insert(int val, node *parent)
 
 void inorder(node *root)
 {
-    // cout<<root->val<<"\n";
-    if(root->right!=nullptr)
-        inorder(root->right);
     cout<<root->val<<"\n";
-    if(root->left!=nullptr)
+    if (root->right != nullptr)
+        inorder(root->right);
+    // cout << root->val << "\n";
+    if (root->left != nullptr)
         inorder(root->left);
     // cout<<root->val<<"\n";
 }
 
-bool search(node* root, int key)
-{   
-    if(root == nullptr)
+bool search_rec(node *root, int key)
+{
+    if (root == nullptr)
         return false;
 
-    if(key == root->val)
+    if (key == root->val)
         return true;
-    else if(key > root->val)
-        return search(root->right, key);
-    else 
-        return search(root->left, key);
+    else if (key > root->val)
+        return search_rec(root->right, key);
+    else
+        return search_rec(root->left, key);
+}
+
+bool search_iter(node *root, int key)
+{
+    while (root != nullptr)
+    {
+        if (key == root->val)
+            return true;
+        else if (key > root->val)
+            root = root->right;
+        else
+            root = root->left;
+    }
+    return false;
+}
+
+int min(node *root)
+{
+    if(root->left!=nullptr)
+        return min(root->left);
+    else
+        return root->val;
+    
+}
+
+int max(node *root)
+{
+    if(root->right!=nullptr)
+        return max(root->right);
+    else
+        return root->val;
+    
 }
 
 int main()
 {
     int a[] = {3, 4, 1, 2, 5, 9, 7};
-    int n = sizeof(a)/sizeof(a[0]);
+    int n = sizeof(a) / sizeof(a[0]);
 
     node root;
     root.val = -1;
@@ -88,10 +120,12 @@ int main()
     root.right = nullptr;
     root.parent = nullptr;
 
-    for(int i=0;i<n; i++)   insert(a[i], &root);
+    for (int i = 0; i < n; i++)
+        insert(a[i], &root);
 
     inorder(&root);
 
-    cout<<"search "<<search(&root, -1)<<"\n";
-
+    cout << "search " << search_iter(&root, 0) << "\n";
+    cout << "minimum " << min(&root) << "\n";
+    cout << "maximum " << max(&root) << "\n";
 }
