f1119fa60dccab0119adde4c8803b81a22b0211f
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-10-25 12:02:46 +0530

    updates

--- codeforces/Educational Codeforces Round 75 (Div 2)/A.cpp
+++ codeforces/Educational Codeforces Round 75 (Div 2)/A.cpp
@@ -10,7 +10,26 @@ int main()
     cin>>t;
     while(t--)
     {
+        string str;
+        cin>>str;
+        set<char> s;
+        int i=0;
 
+        while(i<str.size())
+        {
+            if(i+1<str.size()&&str[i+1]==str[i])
+            {
+                i+=2;
+            }
+            else
+            {
+                s.insert(str[i]);
+                i+=1;
+            }
+        }
+        for(auto x:s)
+            cout<<x;
+        cout<<"\n";
     }
     return 0;
 }
